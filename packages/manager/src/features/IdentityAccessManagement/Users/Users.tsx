8e40b0d888fb52292bed12aa0a72eb7af993ecd5
Anastasiia Alekseenko <aaleksee@akamai.com>
2024-11-12 11:38:04 +0100

    feat: [UIE-8136] - add new users table component

--- packages/manager/src/features/IdentityAccessManagement/Users/Users.tsx
+++ packages/manager/src/features/IdentityAccessManagement/Users/Users.tsx
@@ -1,39 +1,95 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
 
-import { ActionMenu } from 'src/components/ActionMenu/ActionMenu';
+import { Box } from 'src/components/Box';
+import { Button } from 'src/components/Button/Button';
+import { DocumentTitleSegment } from 'src/components/DocumentTitle';
+import { PaginationFooter } from 'src/components/PaginationFooter/PaginationFooter';
+import { Table } from 'src/components/Table';
+import { TableBody } from 'src/components/TableBody';
+import { useOrder } from 'src/hooks/useOrder';
+import { usePagination } from 'src/hooks/usePagination';
+import { useAccountUsers } from 'src/queries/account/users';
+import { DebouncedSearchTextField } from 'src/components/DebouncedSearchTextField';
 
-const user = {
-  username: 'aaleksee-akamai',
-};
+import { UsersLandingTableBody } from '../Users/UsersLandingTableBody';
+import { UsersLandingTableHead } from '../Users/UsersLandingTableHead';
+
+import type { Filter } from '@linode/api-v4';
+import { Paper } from 'src/components/Paper';
 
 export const UsersLanding = () => {
-  const history = useHistory();
+  const pagination = usePagination(1, 'account-users');
+  const order = useOrder();
 
-  const actions: any[] = [
-    {
-      onClick: () => {
-        history.push(
-          `/identity-access-management/users/${user.username}/details`
-        );
-      },
-      title: 'View User Details',
+  const usersFilter: Filter = {
+    ['+order']: order.order,
+    ['+order_by']: order.orderBy,
+  };
+
+  // Since this query is disabled for restricted users, use isLoading.
+  const { data: users, error, isLoading } = useAccountUsers({
+    filters: usersFilter,
+    params: {
+      page: pagination.page,
+      page_size: pagination.pageSize,
     },
-    {
-      onClick: () => {
-        history.push(
-          `/identity-access-management/users/${user.username}/roles`
-        );
-      },
-      title: 'View User Roles',
-    },
-  ];
+  });
+
+  const numCols = 4;
+
+  const handleDelete = (username: string) => {
+    // mock
+  };
+
+  const handleSearch = async (value: string) => {
+    // mock
+  };
 
   return (
-    <>
-      <p>Users Table - UIE-8136 </p>
-
-      <ActionMenu actionsList={actions} ariaLabel={`Action menu for user`} />
-    </>
+    <React.Fragment>
+      <DocumentTitleSegment segment="Users & Grants" />
+      <Paper sx={(theme) => ({ marginTop: theme.spacing(2) })}>
+        <Box
+          sx={(theme) => ({
+            alignItems: 'center',
+            display: 'flex',
+            justifyContent: 'space-between',
+            marginBottom: theme.spacing(2),
+          })}
+        >
+          <DebouncedSearchTextField
+            clearable
+            debounceTime={250}
+            hideLabel
+            label="Filter"
+            onSearch={handleSearch}
+            placeholder="Filter"
+            value=""
+            sx={{ width: 320 }}
+          />
+          <Button buttonType="primary">Add a User</Button>
+        </Box>
+        <Table aria-label="List of Users">
+          <UsersLandingTableHead order={order} />
+          <TableBody>
+            <UsersLandingTableBody
+              error={error}
+              isLoading={isLoading}
+              numCols={numCols}
+              onDelete={handleDelete}
+              users={users?.data}
+            />
+          </TableBody>
+        </Table>
+        <PaginationFooter
+          count={users?.results || 0}
+          eventCategory="users landing"
+          handlePageChange={pagination.handlePageChange}
+          handleSizeChange={pagination.handlePageSizeChange}
+          page={pagination.page}
+          pageSize={pagination.pageSize}
+        />
+      </Paper>
+    </React.Fragment>
   );
 };
